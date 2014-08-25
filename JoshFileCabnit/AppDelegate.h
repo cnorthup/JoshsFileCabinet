d2323b5dc7b74d4adda10b692d6e726bbe2f60ab
Charles Northup <cjnorth40@gmail.com>
2014-08-25 10:27:01 -0500

    Initial Commit

---
+++ JoshFileCabnit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JoshFileCabnit
+//
+//  Created by Charles Northup on 8/25/14.
+//  Copyright (c) 2014 Contract. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
